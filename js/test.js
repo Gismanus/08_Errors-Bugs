c670664a2adeb56138a30a86bbf425f74345d409
Michail Petrov <Gismanus@gmail.com>
2022-09-17 18:29:22 +0300

    Exceptions

--- js/test.js
+++ js/test.js
@@ -5,13 +5,34 @@
 // }
 // console.log(promptNumber('How many trees do you see?'));
 
-function lastElement(array) {
-    if (array.length == 0) {
-        return { failed: true };
-    }
-    else {
-        return { element: array[array.length - 1] };
+// function lastElement(array) {
+//     if (array.length == 0) {
+//         return { failed: true };
+//     }
+//     else {
+//         return { element: array[array.length - 1] };
+//     }
+// }
+// console.log(lastElement([2, 3, 4]))
+// console.log(lastElement([]))
+
+function promptDirection(question) {
+    let result = prompt(question);
+    if (result.toLowerCase() == 'left') return 'L';
+    if (result.toLowerCase() == 'right') return 'R';
+    throw new Error('failed direction: ' + result);
+}
+
+function look(){
+    if (promptDirection('Where to go?') == 'L'){
+        return 'home';
+    } else {
+        return 'two angry bears';
     }
 }
-console.log(lastElement([2, 3, 4]))
-console.log(lastElement([]))
+
+try {
+    console.log('In front of you', look());
+} catch (error) {
+    cconsole.log('Something went wrong: ' + error);
+}
