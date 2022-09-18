301950bcd03574cf4187c79aa390a8ecc086d1cd
Michail Petrov <Gismanus@gmail.com>
2022-09-18 18:57:23 +0300

    add requests

--- js/badCode.js
+++ js/badCode.js
@@ -3,8 +3,9 @@ const accounts = {
     b: 0,
     c: 20
 };
+console.log(accounts);
 function getAccount(){
-    let accountName = prompt('Enter login');
+    let accountName = prompt('Enter recipient login');
     if (!accounts.hasOwnProperty(accountName)){
         throw new Error(`No such account: ${accountName}`);
     }
@@ -16,3 +17,7 @@ function transfer(from, amount) {
     accounts[from] -= amount;
     accounts[getAccount()] += amount;
 }
+
+// transfer(b, 100);
+transfer(prompt('Enter sender login'), Number(prompt('Enter amount of money')));
+console.log(accounts);
