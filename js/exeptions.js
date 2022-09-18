b48da65dbf969047646c1bf97d2b8d051cbefb49
Michail Petrov <Gismanus@gmail.com>
2022-09-19 01:31:20 +0300

    special Values for debugging

--- js/exeptions.js
+++ js/exeptions.js
@@ -1,21 +1,3 @@
-// function promptNumber(question) {
-//     let result = Number(prompt(question));
-//     if (Number.isNaN(result)) return null;
-//     else return result;
-// }
-// console.log(promptNumber('How many trees do you see?'));
-
-// function lastElement(array) {
-//     if (array.length == 0) {
-//         return { failed: true };
-//     }
-//     else {
-//         return { element: array[array.length - 1] };
-//     }
-// }
-// console.log(lastElement([2, 3, 4]))
-// console.log(lastElement([]))
-
 function promptDirection(question) {
     let result = prompt(question);
     if (result.toLowerCase() == 'left') return 'L';
